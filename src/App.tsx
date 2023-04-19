d98fbf1b9f2f3c815c3049e1152c080538ae23c3
ikjun <harry960629@naver.com>
2023-04-19 13:54:38 +0900

    edit App.tsx import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import './App.css';
+// import './App.css';
 import AppRouter from './router/AppRouter';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { cartActions } from './store/cart';
 import { useAppDispatch } from './store';
 import { fetchProducts } from './store/product';
 import { createContext, useState, useEffect } from 'react';
